5b2be04e1e02451ce80f7256410574ffdfd68a43
fabian <fabiankmroh@gmail.com>
2020-07-06 23:39:44 +0900

    20200706 Work from Ubuntu

--- baekjoon/17611.cpp
+++ baekjoon/17611.cpp
@@ -4,6 +4,8 @@
 int lineSE[1000000] = { 0, };
 int lineCnt[1000000] = { 0, };
 int x[100000][2];
+int xmin = 500000;
+int xmax = -500000;
 int n, i;
 
 int main(void){
@@ -11,11 +13,14 @@ int main(void){
 
     for(i = 0; i < n; i++){
         scanf("%d %d", &c[i][0], &c[i][1]);
-    }
 
-    for(i = 0; i < n; i++){
-        lineSE[x[i][0]]++;
-        lineSE[x[i][1]]--;
+        // Min
+        if(c[i][0] < xmin)
+            xmin = c[i][0];
+        
+        // Max
+        if(c[i][0] > xmax)
+            xmax = c[i][0];
     }
 
     
