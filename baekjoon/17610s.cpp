2570819c29ede8676cf46a98eee4b9da5eb58c30
Fabian <fabiankmroh@gmail.com>
2020-07-06 23:13:47 +0900

    20200706 Independent Study Session

--- baekjoon/17610s.cpp
+++ baekjoon/17610s.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #define MIDDLE  260000
+#define DEBUG   1
 
 using namespace std;
 
@@ -10,8 +11,8 @@ int k, ans = 0;
 int sum = 0;
 
 void print(void){
-    for(i = 1; i <= sum; i++){
-        printf("possible[%d] = %d\n", i, possible[i+sum]);
+    for(i = -sum; i <= sum; i++){
+        printf("possible[%d] = %d\n", i, possible[i+MIDDLE]);
     }
     printf("\n\n");
 
@@ -19,17 +20,25 @@ void print(void){
 }
 
 void makeOne(int n){
+    possible[weight[n]+MIDDLE] = n;
+    possible[MIDDLE-weight[n]] = n;
+
     for(j = MIDDLE-sum; j <= MIDDLE+sum; j++){
         if(possible[j] < n && possible[j] > 0){
-            possible[j+weight[n]] = n;
-            printf("possible[%d] = %d\n", j+weight[n], n);
+            if(possible[j+weight[n]] == 0)
+                possible[j+weight[n]] = n;
+            if(DEBUG)
+                printf("possible[%d+%d] = %d\n", j-MIDDLE, weight[n], n);
 
             possible[j-weight[n]] = n;
-            printf("possible[%d] = %d\n", j-weight[n], n);
+            if(DEBUG)
+                printf("possible[%d-%d] = %d\n", j-MIDDLE, weight[n], n);
         }
     }
-    printf("\n");
-    print();
+    if(DEBUG){
+        printf("\n");
+        print();
+    }
 
     return;
 }
@@ -42,28 +51,53 @@ int main(void){
         sum += weight[i];
     }
     
-    possible[weight[0]+sum] = 1;
-    printf("possible[%d] = %d\n", weight[0], 1);
+    possible[weight[0]+MIDDLE] = 1;
+    possible[MIDDLE-weight[0]] = 1;
 
-    possible[weight[0]-weight[1]+sum] = 1;
-    printf("possible[%d] = %d\n", weight[0]-weight[1], 1);
+    if(DEBUG){
+        printf("possible[%d] = %d\n", weight[0], 1);
+        printf("possible[%d] = %d\n", -weight[0], 1);
+        printf("\n\n");
+    }
 
-    possible[weight[0]+weight[1]+sum] = 1;
-    printf("possible[%d] = %d\n", weight[0]+weight[1], 1);
+    possible[weight[1]+MIDDLE] = 1;
+    possible[MIDDLE-weight[1]] = 1;
 
-    printf("\n\n");
+    if(DEBUG){
+        printf("possible[%d] = %d\n", weight[1], 1);
+        printf("possible[%d] = %d\n", -weight[1], 1);
+    }
 
-    print();
+    possible[weight[0]-weight[1]+MIDDLE] = 1;
+    possible[weight[0]+weight[1]+MIDDLE] = 1;
+
+    if(DEBUG){
+        printf("possible[%d] = %d\n", weight[0]-weight[1], 1);
+        printf("possible[%d] = %d\n", weight[0]+weight[1], 1);
+    }
+
+    possible[MIDDLE-weight[0]-weight[1]] = 1;
+    possible[MIDDLE-weight[0]+weight[1]] = 1;
+
+    if(DEBUG){
+        printf("possible[%d] = %d\n", MIDDLE-weight[0]-weight[1], 1);
+        printf("possible[%d] = %d\n", MIDDLE-weight[0]+weight[1], 1);
+        printf("\n\n");
+        print();
+    }
 
     for(i = 2; i < k; i++){
-        printf("makeOne(%d)\n", i);
+        if(DEBUG)
+            printf("makeOne(%d)\n", i);
         makeOne(i);
     }
 
     for(i = 1; i <= sum; i++){
-        if(possible[i] == 0)
+        if(possible[i+MIDDLE] == 0)
             ans++;
     }
 
     printf("%d\n", ans);
+
+    return 0;
 }
--- baekjoon/17611.cpp
+++ baekjoon/17611.cpp
@@ -1,77 +1,22 @@
 #include <stdio.h>
-#include <vector>
-#include <algorithm>
+#define ZERO    500000
 
-#define DEBUG 0
-
-using namespace std;
-
-int n;
-vector <pair<int, int> > h;
-vector <pair<int, int> > v;
-int data[100010][2];
-int horiz[1000010]; 
-int verti[1000010]; 
+int lineSE[1000000] = { 0, };
+int lineCnt[1000000] = { 0, };
+int x[100000][2];
+int n, i;
 
 int main(void){
-   scanf("%d", &n);
+    scanf("%d", &n);
 
-   for(int i = 0; i < n; i++){
-       scanf("%d %d", &data[i][0], &data[i][1]);
-   }
+    for(i = 0; i < n; i++){
+        scanf("%d %d", &c[i][0], &c[i][1]);
+    }
 
-   data[n][0] = data[0][0];
-   data[n][1] = data[0][1];
-   
-   for(int i = 0; i < n; i++){
-       if(data[i][0] == data[i+1][0]){
-           if(data[i][1] > data[i+1][1]){
-               v.push_back(make_pair(data[i+1][1], data[i][1]));
-           }
-           else{
-               v.push_back(make_pair(data[i][1], data[i+1][1]));
-           }
-       }
-       
-       if(data[i][1] == data[i+1][1]){
-           if(data[i][0] > data[i+1][0]){
-               h.push_back(make_pair(data[i+1][0], data[i][0]));
-           }
-           else{
-               h.push_back(make_pair(data[i][0], data[i+1][0]));
-           }
-       }
-   }
+    for(i = 0; i < n; i++){
+        lineSE[x[i][0]]++;
+        lineSE[x[i][1]]--;
+    }
 
-   for(int i = 0; i < h.size(); i++){
-       int left = h[i].first + 500000;
-       int right = h[i].second + 500000;
-       
-       horiz[left]++;
-       horiz[right]--;
-   }
-   
-   for(int i = 0; i < v.size(); i++){
-       int down = v[i].first + 500000;
-       int up = v[i].second + 500000;
-       verti[down]++;
-       verti[up]--;
-   }
-   
-   int cnt = 0;
-   int max = 0;
-   for(int i = 0; i < 1000005; i++){
-       cnt += horiz[i];
-       max = max < cnt ? cnt : max;
-   }
-
-   cnt = 0;
-   for(int i = 0; i < 1000005; i++){
-       cnt += verti[i];
-       max = max < cnt ? cnt : max;
-   }
-   
-   printf("%d\n", max);
-   
-   return 0;
+    
 }
