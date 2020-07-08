49e62d195c72d378f35247466458a3964b5a920f
fabian <fabiankmroh@gmail.com>
2020-07-08 16:39:07 +0900

    20200708: Failed to Solve 15971

--- baekjoon/15971.cpp
+++ baekjoon/15971.cpp
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <algorithm>
 
 using namespace std;
 
+vector< pair<int, int> > pipes;
+int holes, i;
+int rob1pos, rob2pos;
+int s, e, l;
+
+void solve(int cPos, int cost, int longest){
+    
+}
+
 int main(void){
-    int start, end, len;
+    scanf("%d %d %d", &holes, &rob1pos, &rob2pos);
+
+    if(rob1pos == rob2pos){
+        printf("0\n");
+        return 0;
+    }
+
+    for(i = 0; i < holes; i++){
+        scanf("%d %d %d", &s, &e, &l);
+        pipes.push_back(make_pair(s, l));
+        pipes.push_back(make_pair(e, l));
+    }
 }
